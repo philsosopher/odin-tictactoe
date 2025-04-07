045b61e2f03efb687264e03e856475b220de1fd5
Utkarsh <utkr200@gmail.com>
2025-04-08 00:54:09 +0530

    improving code to align with assignment instructions - 2

--- script.js
+++ script.js
@@ -5,7 +5,12 @@
  * -1 -> empty
  */
 
-const gameBoard = (function() {
+function Player(name, marker) {
+    let score = 0;
+    return { name, marker, score };
+}
+
+const Gameboard = (function() {
     const grid = Array(3).fill().map(() => Array(3).fill(-1));
 
     const print = () => grid.forEach(row => console.log(row.join(" ")));
@@ -15,141 +20,173 @@ const gameBoard = (function() {
         if (grid[i][j] === -1)
             grid[i][j] = value;
     };
-    const isFull = () => grid.forEach(row => row.every((cell) => cell !== -1));
-
-    return { print, reset, getCell, setCell, isFull };
+    const isFull = () => grid.every(row => row.every((cell) => cell !== -1));
+    const getIndex = (index) => ({i: Math.floor(index / 3), j: index % 3});
+    return { print, reset, getCell, setCell, isFull, getIndex };
 })();
 
-const displayController = (function() {
-
-})();
-
-const game = (function() {
-    // initialize grid with -1
+const DisplayController = (function() {
+    const gridDom = document.querySelector(".grid");
+    const p1score = document.querySelector(".player1 .score");
+    const p2score = document.querySelector(".player2 .score");
+    const p1name = document.querySelector(".player1 .name");
+    const p2name = document.querySelector(".player2 .name");
+    const turnIndicator = document.querySelector("main .turn");
     
-    let gameOver = true;
-    let currMove = 1; // current turn 1 or 0
-
-    // Methods
-    
-    const printGrid = () => {
-        grid.forEach(row => {
-            console.log(row.join(" "));
-            console.log('\n');
+    // render grid dom based on gameboard
+    const renderBoard = () => {
+        Array.from(gridDom.children).forEach((cell, index) => {
+            const {i, j} = Gameboard.getIndex(index);
+            const value = Gameboard.getCell(i, j);
+            cell.textContent = value === 1 ? "X" : value === 0 ? "O" : "";
         });
     };
-
-    const resetGrid = (gridDom) => {
-        grid.forEach(row => row.fill(-1));
-        Array.from(gridDom.children).forEach(e => e.textContent = "");
+    // fn to update player scores
+    const updateScore = (players) => {
+        p1score.textContent = players[0].score;
+        p2score.textContent = players[1].score;
     };
+    // fn to update player Details
+    const updateName = (players) => {
+        p1name.textContent = players[0].name;
+        p2name.textContent = players[1].name;
+    };
+    // fn to update turn indicator message
+    const updateMessage = (message) => { turnIndicator.textContent = message };
 
-    const getIndex = (index) => {
-        return { i: Math.floor((index) / 3), j: (index) % 3};
-    }
+    return {renderBoard, updateScore, updateName, updateMessage };
+})();
 
-    /**
-     * 
-     * @param {dom object} cell - the HTML DOM cell node
-     * @returns evaluate() if valid move, -2 otherwise
-     */
-    const play = (cell, gridDom, turnIndicator) => {
-        if (gameOver) {
-            console.log("Please start a new game!");
+const Game = (function() {
+    // game state variables
+    let turn = 1;
+    let matchOver = true;
+    const players = [
+        Player("Player O", "O"),
+        Player("Player X", "X")
+    ];
+
+    const startGame = (playerO, playerX) => {
+        // set player details
+        players[0].name = playerO;
+        players[1].name = playerX;
+        // set player name doms
+        DisplayController.updateName(players);
+        // set player scores to 0
+        players[0].score = players[1].score = 0;
+        // set player score doms
+        DisplayController.updateScore(players);
+        // start match
+        startMatch();
+    };
+    const startMatch = () => {
+        matchOver = false;
+        // clear gameboard
+        Gameboard.reset();
+        // re-render grid Dom
+        DisplayController.renderBoard();
+        // set turn to 1 i.e X
+        turn = 1;
+        // set turn indicator message
+        DisplayController.updateMessage(`${players[turn].name}'s turn`);
+    };
+    const endMatch = (won) => {
+        matchOver = true;
+        // handle case where won === -1 -> Tie
+        if (won === -1) {
+            DisplayController.updateMessage(`This match was a tie!`);
             return;
         }
+        // update score of won player
+        players[won].score++;
+        console.log(players[won]);
+        // update score dom
+        DisplayController.updateScore(players);
+        // update turn indicator message to Player[won] won
+        DisplayController.updateMessage(`${players[won].name} won!!`);
+    }
+    const play = (index) => {
+        if(matchOver) return;
 
-        const cellIndex = Array.from(gridDom.children).indexOf(cell);
+        const {i, j} = Gameboard.getIndex(index);
 
-        let {i, j} = getIndex(cellIndex);
-        console.log(i,j);
+        // update Gameboard and dom
+        if (Gameboard.getCell(i, j) === -1) {
+            Gameboard.setCell(i, j, turn);
+            DisplayController.renderBoard();
+            // log grid on console
+            Gameboard.print();
 
-        let res = -2; // invalid move
+            // evaluate if match is over
+            let won = evaluate();
+            if (won !== -1 || Gameboard.isFull()) {
+                endMatch(won);
+                return;
+            } 
 
-        if (grid[i][j] === -1) {
-            grid[i][j] = currMove;
-            let charMove = currMove === 0 ? "O" : "X";
-            currMove = currMove === 0 ? 1 : 0;
-            turnIndicator.textContent = `Player ${charMove}'s turn`;
-            cell.textContent = charMove;
-            res = evaluate();
-        }
+            // update turn
+            turn = turn === 0 ? 1 : 0;
 
-        printGrid();
-
-        if (res === 0) {
-            stopGame("O", turnIndicator);
-        } else if (res === 1) {
-            stopGame("X", turnIndicator);
-        } else if (res === -2) {
+            // update turn message
+            DisplayController.updateMessage(`${players[turn].name}'s turn`);
+        } else {
             alert("Invalid Move");
         }
     };
-
+    
     /**
-     * 
      * @returns 1 (if 1 won), 0 (if 0 won), -1 otherwise
      */
     const evaluate = () => {
+        const grid = Gameboard.getCell;
         for (let i = 0; i < 3; i++) {
-            // check row i
-            if (grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2] && grid[i][0] !== -1) {
-                return grid[i][0];
-            }
-
-            // check column i
-            if (grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i] && grid[0][i] !== -1) {
-                return grid[0][i];
-            }
+            if (grid(i, 0) === grid(i, 1) && grid(i, 1) === grid(i, 2) && grid(i, 0) !== -1) 
+                return grid(i, 0);
+            if (grid(0, i) === grid(1, i) && grid(1, i) === grid(2, i) && grid(0, i) !== -1) 
+                return grid(0, i);
         }
-
-        if (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2] && grid[0][0] !== -1) {
-            return grid[0][0];
-        }
-
-        if (grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0] && grid[0][2] !== -1) {
-            return grid[0][2];
-        }
-
+        if (grid(0, 0) === grid(1, 1) && grid(1, 1) === grid(2, 2) && grid(0, 0) !== -1) 
+            return grid(0, 0);
+        if (grid(0, 2) === grid(1, 1) && grid(1, 1) === grid(2, 0) && grid(0, 2) !== -1) 
+            return grid(0, 2);
         return -1;
     };
 
-    const startGame = (turnIndicator, gridDom) => {
-        gameOver = false;
-        resetGrid(gridDom);
-        turnIndicator.textContent = "Player X's turn";
-    };
-
-    const stopGame = (player, turnIndicator) => {
-        gameOver = true;
-        turnIndicator.textContent = `Player ${player} won 🎉`;
-    };
-    
-    // export/return object with public methods
-    return {
-        play,
-        startGame
-    };
-    
+    return { startGame, startMatch, endMatch, play };
 })();
-
 
 function main() {
     // dom elements
-    const turnIndicator = document.querySelector("main .turn");
+    const dialogBox = document.querySelector(".player-names");
+    const dialogSubmitButton = document.querySelector("form .start-button");
     const gridDom = document.querySelector("main .grid");
-    const restartButton = document.querySelector("main .restart");
+    const restartGameButton = document.querySelector("main .restart-game");
+    const restartMatchButton = document.querySelector("main .restart-match");
 
+    Gameboard.reset();
+    DisplayController.renderBoard();
     gridDom.addEventListener("click", (e) => {
-        // alert(e.target.textContent);
-        game.play(e.target, gridDom, turnIndicator);
+        const children = Array.from(gridDom.children);
+        const index = children.indexOf(e.target);
+        Game.play(index);
     });
-    game.startGame(turnIndicator, gridDom);
-    restartButton.addEventListener("click", () => {
-        game.startGame(turnIndicator, gridDom);
+    restartGameButton.addEventListener("click", () => dialogBox.showModal());
+    dialogSubmitButton.addEventListener("click", (event) => {
+        event.preventDefault(); // prevent default action of form sending data to server
+
+        restartMatchButton.addEventListener("click", () => Game.startMatch());
+
+        const playerO = document.getElementById("playero").value.trim();
+        const playerX = document.getElementById("playerx").value.trim();
+
+        Game.startGame(playerO, playerX);
+
+        // reset form
+        event.target.reset();
+
+        // close dialog
+        dialogBox.close();
     });
-
-
 }
 
 main();
